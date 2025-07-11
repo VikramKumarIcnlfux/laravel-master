a0b451b0e46a7a3f814621c32e892a5b5e251ba1
Vikram <vikram.kumar@iconflux.com>
2025-07-11 14:53:54 +0530

    controller update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,14 +9,6 @@ class UserController extends Controller
 {
     public function show(Request $request)
     {
-        $user = User::find(1);
-
-        // ❌ ERROR 1: Calling undefined method
-        $user->nonExistentMethod();
-
-        // ❌ ERROR 2: Accessing undefined property
-        $email = $user->non_existing_property;
-
-        return response()->json(['email' => $email]);
+        
     }
 }
